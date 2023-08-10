1d7120d75e4240eb6f8f5dc578f43a243aacbd4e
RavshanbekIsroilov <isroilovravshan42@gmail.com>
2023-08-10 13:22:45 +0500

    add repository

---
+++ Data/IRepository/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Commons;
+
+namespace Nebula.DAL.IRepositories;
+
+public interface IRepository<T> where T : Auditable
+{
+    Task CreateAsync(T entity);
+    void Update(T entity);
+    void Delete(T entity);
+    Task<T> SelectByIdAsync(long id);
+    IQueryable<T> SelectAll();
+}
